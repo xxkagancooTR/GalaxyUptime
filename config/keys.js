c21a9d3134a4599862be25218b80ed8c0f2d35c2
xxkagancooTR <kagangokmen31@gmail.com>
2022-04-08 22:32:47 +0300

    s

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongo adresi';
+dbPassword = 'mongodb+srv://xxkagancooTR:kg123456**@cluster0.n0zhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
